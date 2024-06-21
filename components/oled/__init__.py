2048fd2e438fcb4990c17cecef73b463b5988324
mohankumargupta <mohankumargupta@users.noreply.github.com>
2024-06-21 17:49:59 +1000

    oled: add oled index field

--- components/oled/__init__.py
+++ components/oled/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_SDA, CONF_SCL, CONF_ADDRESS
+from esphome.const import CONF_ID, CONF_SDA, CONF_SCL, CONF_ADDRESS, CONF_INDEX
 from esphome import pins
 
 empty_component_ns = cg.esphome_ns.namespace('oled')
@@ -10,13 +10,28 @@ CONF_OLED = "OledComponent"
 
 MULTI_CONF = True
 
+INDEXES = set()
+
+def validate_index(value):
+    value = cv.int_(value)
+    if value < 1 or value > 9:
+        raise cv.Invalid("Index must be between 1 and 9")
+    if value in INDEXES:
+        raise cv.Invalid("Index must be unique")
+    INDEXES.add(value)
+    return value    
+
 CONFIG_SCHEMA = cv.Schema({
     #cv.GenerateID(): cv.declare_id(OledComponent),
     cv.Required(CONF_ID): cv.declare_id(OledComponent), 
     cv.Required(CONF_SDA): pins.internal_gpio_output_pin_number,
     cv.Required(CONF_SCL): pins.internal_gpio_output_pin_number,
+    cv.Required(CONF_INDEX, msg="missing field index"): validate_index,
     cv.Optional(CONF_ADDRESS, default=0x3C): cv.i2c_address, 
+
 }).extend(cv.COMPONENT_SCHEMA)
+
+
 
 def to_code(config):
     cg.add_build_flag("-DU8X8_NO_HW_SPI")
